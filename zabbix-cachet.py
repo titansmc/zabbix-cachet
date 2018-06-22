52b2d2bede9127b59b2dd87ee292390342c72095
root <root@projects.embl.de>
2018-06-22 16:03:26 +0200

    asdf

--- zabbix-cachet.py
+++ zabbix-cachet.py
@@ -398,7 +398,7 @@ class Cachet:
         if components_gr_id['id'] == 0:
             url = 'components/groups'
             # TODO: make if possible to configure default collapsed value
-            params = {'name': name, 'collapsed': 2}
+            params = {'name': name, 'collapsed': 1}
             logging.debug('Creating Component Group {}...'.format(params['name']))
             data = self._http_post(url, params)
             if 'data' in data:
